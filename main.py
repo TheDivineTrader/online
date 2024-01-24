d36391887b5093f2d1a0fedc4306b55886f0a182
TheDivineTrader <104659055+TheDivineTrader@users.noreply.github.com>
2024-01-23 17:07:38 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+import sys
+import json
+import time
+import requests
+import websocket
+
+status = "dnd"
+
+custom_status = "cheap services @ .gg/avix" #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
+
+usertoken = "Nzk3NTAzMDI5OTI2Mjk3NjEw.GcBNCy.sBYYvQGJqxnJWPdimOubi61elvN1cqY8g7pLk0"
+
+headers = {"Authorization": usertoken, "Content-Type": "application/json"}
+
+validate = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers)
+if validate.status_code != 200:
+  print("[ERROR] Your token might be invalid. Please check it again.")
+  sys.exit()
+
+userinfo = requests.get('https://discordapp.com/api/v9/users/@me', headers=headers).json()
+username = userinfo["username"]
+discriminator = userinfo["discriminator"]
+userid = userinfo["id"]
+
+def onliner(token, status):
+    ws = websocket.WebSocket()
+    ws.connect("wss://gateway.discord.gg/?v=9&encoding=json")
+    start = json.loads(ws.recv())
+    heartbeat = start["d"]["heartbeat_interval"]
+    auth = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "properties": {
+                "$os": "Windows 10",
+                "$browser": "Google Chrome",
+                "$device": "Windows",
+            },
+            "presence": {"status": status, "afk": False},
+        },
+        "s": None,
+        "t": None,
+    }
+    ws.send(json.dumps(auth))
+    cstatus = {
+        "op": 3,
+        "d": {
+            "since": 0,
+            "activities": [
+                {
+                    "type": 4,
+                    "state": custom_status,
+                    "name": "Custom Status",
+                    "id": "custom",
+                    "emoji": {
+                        "name": "aPurpleCrown",
+                        "id": "1192646881117999124",
+                        "animated": True,
+                    },
+                }
+            ],
+            "status": status,
+            "afk": False,
+        },
+    }
+    ws.send(json.dumps(cstatus))
+    online = {"op": 1, "d": "None"}
+    time.sleep(heartbeat / 1000)
+    ws.send(json.dumps(online))
+
+def run_onliner():
+  print(f"Logged in as {username}# ({userid}).")
+  while True:
+    onliner(usertoken, status)
+    time.sleep(30)
+
+run_onliner()
